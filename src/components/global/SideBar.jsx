cf952aafcfc218e6063d273fb57ef7d8c2c50479
tein <kerp4real@gmail.com>
2024-04-01 14:30:56 +0100

    links for side bar

--- src/components/global/SideBar.jsx
+++ src/components/global/SideBar.jsx
@@ -1,24 +1,52 @@
 import { faSignOut, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SideBar() {
   const navs = [
-    { id: 1, name: "Profile", image: "../../../src/assets/profile-nav.svg" },
-    { id: 2, name: "Services", image: "../../../src/assets/services.svg" },
-    { id: 3, name: "Billing", image: "../../../src/assets/billing.svg" },
-    { id: 4, name: "Settings", image: "../../../src/assets/settings.svg" },
-    { id: 5, name: "Help", image: "../../../src/assets/help.svg" },
+    {
+      id: 1,
+      name: "Profile",
+      image: "../../../src/assets/profile-nav.svg",
+      link: "/profile",
+    },
+    {
+      id: 2,
+      name: "Services",
+      image: "../../../src/assets/services.svg",
+      link: "/services",
+    },
+    {
+      id: 3,
+      name: "Billing",
+      image: "../../../src/assets/billing.svg",
+      link: "/billing",
+    },
+    {
+      id: 4,
+      name: "Settings",
+      image: "../../../src/assets/settings.svg",
+      link: "/settings",
+    },
+    {
+      id: 5,
+      name: "Help",
+      image: "../../../src/assets/help.svg",
+      link: "/help",
+    },
   ];
   return (
     <>
       <div className="">
         <div className="pt-[8%]">
           {navs.map((item) => (
-            <div className="flex my-[10%]">
-              <img src={item.image} alt="" className="w-[25px] h-[25px]" />
-              <p className="text-[20px] ml-[5%]">{item.name}</p>
-            </div>
+            <Link to={item.link}>
+              <div className="flex my-[10%]">
+                <img src={item.image} alt="" className="w-[25px] h-[25px]" />
+                <p className="text-[20px] ml-[5%]">{item.name}</p>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
