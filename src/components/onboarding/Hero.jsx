5762aa734f6dc7a27d5f420bece96f727f966513
tein <kerp4real@gmail.com>
2024-05-25 20:46:55 +0100

    fixing proxy server issue, and images not showing

--- src/components/onboarding/Hero.jsx
+++ src/components/onboarding/Hero.jsx
@@ -10,15 +10,14 @@ export default function Hero() {
   const slides = [
     {
       id: "1",
-      image: "../../../public/onboard1.png",
+      image: "/onboard1.png",
       text: "Experience convenience and freshness like never before.",
       class: "absolute top-[10%] left-[0px] scale-[1.4] ",
       textStyle: "mt-[100%]",
     },
-
     {
       id: "2",
-      image: "../../../public/onboard2.png",
+      image: "/onboard2.png",
       text: "Simplify Your Life with Seamless Laundry Pickup and Delivery.",
       class: "scale-[1.4] absolute top-[10%] left-[90px]",
       textStyle: "mt-[100%]",
@@ -26,7 +25,7 @@ export default function Hero() {
 
     {
       id: "3",
-      image: "../../../public/onboard3.png",
+      image: "/onboard3.png",
       text: "Eco-Friendly Cleaning that Cares  for Your Clothes and the Planet",
       class: "scale-[1.25] absolute top-[-120px] left-[80px]",
       textStyle: "mt-[100%]",
@@ -34,7 +33,7 @@ export default function Hero() {
 
     {
       id: "4",
-      image: "../../../public/onboard4.png",
+      image: "/onboard4.png",
       text: "Join Us and Transform the Way You Do Laundry!",
       class: "absolute top-[10%] left-[0px] scale-[1.4] mb-[115px]",
       textStyle: "mt-[100%]",
