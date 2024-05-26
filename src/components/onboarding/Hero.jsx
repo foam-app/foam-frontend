e1f1213e40f97bdc18eaec115e942e0277fc0670
tein <kerp4real@gmail.com>
2024-05-26 15:46:03 +0100

    slideshow components improved

--- src/components/onboarding/Hero.jsx
+++ src/components/onboarding/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Slideshow from "./Slideshow";
 
 import { motion } from "framer-motion";
@@ -6,43 +6,61 @@ import { motion } from "framer-motion";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 
+const usePreloadImages = (imageUrls) => {
+  useEffect(() => {
+    imageUrls.forEach((url) => {
+      const img = new Image();
+      img.src = url;
+    });
+  }, [imageUrls]);
+};
+
 export default function Hero() {
   const slides = [
     {
       id: "1",
-      image: "/onboard1.png",
+      image: "/onboard1.webp",
       text: "Experience convenience and freshness like never before.",
-      class: "absolute top-[10%] left-[0px] scale-[1.4] ",
+      class: "absolute top-[10%] left-[0px]",
       textStyle: "mt-[100%]",
     },
     {
       id: "2",
-      image: "/onboard2.png",
+      image: "/onboard2.webp",
       text: "Simplify Your Life with Seamless Laundry Pickup and Delivery.",
-      class: "scale-[1.4] absolute top-[10%] left-[90px]",
+      class: "absolute top-[50px] left-[95px] ",
       textStyle: "mt-[100%]",
     },
 
     {
       id: "3",
-      image: "/onboard3.png",
+      image: "/onboard3.webp",
       text: "Eco-Friendly Cleaning that Cares  for Your Clothes and the Planet",
-      class: "scale-[1.25] absolute top-[-120px] left-[80px]",
+      class: "absolute top-[-90px] left-[125px]",
       textStyle: "mt-[100%]",
     },
 
     {
       id: "4",
-      image: "/onboard4.png",
+      image: "/onboard4.webp",
       text: "Join Us and Transform the Way You Do Laundry!",
-      class: "absolute top-[10%] left-[0px] scale-[1.4] mb-[115px]",
+      class: "absolute top-[10%] left-[0px] mb-[115px]",
       textStyle: "mt-[100%]",
     },
   ];
 
+  const imagesToPreload = [
+    "/onboard1.webp",
+    "/onboard2.webp",
+    "/onboard3.webp",
+    "/onboard4.webp",
+  ];
+
+  usePreloadImages(imagesToPreload);
+
   return (
     <>
-      <div className="gradient-bg  w-[100%] px-[2.5%]">
+      <div className="gradient-bg w-[100%] px-[2.5%] h-[100vh]">
         <div className="w-[100%] py-[5%] px-[2.5%]">
           <img src={logo} alt="" />
         </div>
--- src/components/onboarding/Slideshow.jsx
+++ src/components/onboarding/Slideshow.jsx
@@ -21,102 +21,90 @@ export default function Slideshow({ slides }) {
 
   return (
     <>
-      {/* <div className="relative">
-                <div className="flex justify-center items-center">
-                    <div className="h-[400px]">
-                            <div className="flex justify-center items-center">
-                                <motion.div
-                                    key={slides[currentSlide].id}
-                                    initial={{ opacity: 0.3, x: 100 }}
-                                    animate={{ opacity: 1, x: 0 }}
-                                    exit={{ opacity: 0, x: -300 }}
-                                    transition={{ duration: 0.5 }}
-                                    onAnimationComplete={() => setIsTransitioning(false)}
-                                    >
-                                        <div className=''>
-                                            <img src={slides[currentSlide].image} alt="slide" className={slides[currentSlide].class}/>
-                                        </div>
-                                </motion.div>
-                            </div>
-
-                            <div className="text-[23px] font-medium text-center">
-                                <p className={slides[currentSlide].textStyle}>{slides[currentSlide].text}</p>
-                            </div>
-
-                            
-                            <div className="fixed top-[42.5%] left-[158px] mt-20 flex justify-center items-center">
-                                    <div className="flex">
-                                        {slides.map((_, index) => (
-                                            <div
-                                                key={index}
-                                                className={`mx-1 w-[8px] h-[8px] rounded-full ${index === currentSlide ? 'bg-[#001C1F]' : 'bg-[#C8FAFF]'}`}
-                                                onClick={() => setCurrentSlide(index)}
-                                            />
-
-                                        ))}
-                                    </div>
-                            </div>
-                    </div>
-                </div>
-        </div> */}
-
-      <div className="">
-        <div className="onboard">
-          <div className="flex justify-center items-center relative mb-[49.5%]">
-            <motion.div
-              key={slides[currentSlide].id}
-              initial={{
-                //opacity: 0.3,
-                x: 100,
-              }}
-              animate={{
-                //opacity: 1,
-                x: 0,
-              }}
-              exit={{
-                //opacity: 0,
-                x: -300,
-              }}
-              transition={{ duration: 0.2 }}
-              onAnimationComplete={() => setIsTransitioning(false)}
-            >
-              <img
-                src={slides[currentSlide].image}
-                alt=""
-                className={slides[currentSlide].class}
-              />
-            </motion.div>
-          </div>
-
-          <div className="my-auto">
-            <div className="flex justify-center items-center mt-[5%] mb-[2%]">
-              {slides.map((_, index) => (
-                <div
-                  key={index}
-                  className={`mx-1 w-[8px] h-[8px] rounded-full ${
-                    index === currentSlide ? "bg-[#001C1F]" : "bg-[#C8FAFF]"
-                  }`}
-                  onClick={() => setCurrentSlide(index)}
+      <motion.div>
+        <div className="">
+          <div className="onboard">
+            <div className="flex justify-center items-center relative mb-[49.5%]">
+              {/* <motion.div
+                key={slides[currentSlide].id}
+                initial={{
+                  //opacity: 0.3,
+                  x: 100,
+                }}
+                animate={{
+                  //opacity: 1,
+                  x: 0,
+                }}
+                exit={{
+                  //opacity: 0,
+                  x: -300,
+                }}
+                transition={{ duration: 0.2 }}
+                onAnimationComplete={() => setIsTransitioning(false)}
+                mb-[49.5%]
+              >
+                <img
+                  src={slides[currentSlide].image}
+                  alt=""
+                  className={slides[currentSlide].class}
                 />
-              ))}
+              </motion.div> */}
+              <div className={slides[currentSlide].class}>
+                <motion.div
+                  key={slides[currentSlide].id}
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -300 }}
+                  transition={{ duration: 0.7 }}
+                  onAnimationComplete={() => setIsTransitioning(false)}
+                >
+                  <div className="">
+                    <img
+                      src={slides[currentSlide].image}
+                      alt=""
+                      className={`${
+                        slides[currentSlide].id == 2 ||
+                        slides[currentSlide].id == 3
+                          ? "scale-[1.8]"
+                          : "scale-[1.4]"
+                      }`}
+                      // className={slides[currentSlide].class}
+                    />
+                  </div>
+                </motion.div>
+              </div>
             </div>
-            <p className="text-[24px] font-medium text-center">
-              {slides[currentSlide].text}
-            </p>
-            <p className="text-center text-[18px] text-[#212828] py-[4%]">
-              Schedule your pickup today
-            </p>
-            <Link to="/signup" className="flex justify-center items-center">
-              <button className="w-[98%] font-bold rounded-[8px] py-[5%]  text-[17px] text-white bg-[#001C1F]">
-                Sign me up!
-              </button>
-            </Link>
-            <p className="text-center text-[15px] text-[#212828] py-[3%]">
-              <Link to="/login">Log in with your exisiting account</Link>
-            </p>
+
+            <div className="my-auto">
+              <div className="flex justify-center items-center mt-[9%] mb-[2%]">
+                {slides.map((_, index) => (
+                  <div
+                    key={index}
+                    className={`mx-1 w-[8px] h-[8px] rounded-full ${
+                      index === currentSlide ? "bg-[#001C1F]" : "bg-[#C8FAFF]"
+                    }`}
+                    onClick={() => setCurrentSlide(index)}
+                  />
+                ))}
+              </div>
+              <p className="text-[24px] font-medium text-center">
+                {slides[currentSlide].text}
+              </p>
+              <p className="text-center text-[18px] text-[#212828] py-[4%]">
+                Schedule your pickup today
+              </p>
+              <Link to="/signup" className="flex justify-center items-center">
+                <button className="w-[98%] font-bold rounded-[8px] py-[5%]  text-[17px] text-white bg-[#001C1F]">
+                  Sign me up!
+                </button>
+              </Link>
+              <p className="text-center text-[15px] text-[#212828] py-[3%]">
+                <Link to="/login">Log in with your exisiting account</Link>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
